6419630bf01642c92007adc5040f6f5f10455937
JHiltunen <joonas.s.jh@gmail.com>
2018-10-10 09:36:50 +0300

    Added more integration tests to test api

--- test/integration/test-route-bmi.js
+++ test/integration/test-route-bmi.js
@@ -14,3 +14,39 @@ exports.bmi_should_reject_strings = function (done) {
     .expect(400)
     .end(done);
 };
+
+exports.bmi_should_work_in_finnish_and_metric = function (done) {
+    supertest(app)
+    .get('/bmi?weight=70&height=2.0&lang=fi&unit=metric')
+    .expect(200, {
+        result: {
+            bmi: 17.5,
+            description: 'Lievä alipaino'
+        }
+    })
+    .end(done);
+};
+
+exports.bmi_should_work_with_metric_in_english = function (done) {
+    supertest(app)
+    .get('/bmi?weight=70&height=2.0&lang=en&unit=metric')
+    .expect(200, {
+        result: {
+            bmi: 17.5,
+            description: 'Mild underweight'
+        }
+    })
+    .end(done);
+};
+
+exports.bmi_should_work_with_imperial_in_english = function (done) {
+    supertest(app)
+    .get('/bmi?weight=66&height=51lang=en&unit=imperial')
+    .expect(200, {
+        result: {
+            bmi: 17.83852364475202,
+            description: 'Mild underweight'
+        }
+    })
+    .end(done);
+};
